325cafbdd07e0c93a76c4f1318eed6d48536d39a
rajkonkret <rajkonkret660@gmail.com>
2019-12-03 12:02:34 +0100

    obiekt receipt

--- src/Bill.java
+++ src/Bill.java
@@ -1,4 +1,5 @@
 public interface Bill {
     void printBill();
-    
+
+
 }
--- src/Run.java
+++ src/Run.java
@@ -1,5 +1,7 @@
 public class Run {
     public static void main(String[] args) {
-
+        Receipt receipt1 = new Receipt(10.0, "Kawa");
+        Bill[] bills = {receipt1};
+        bills[0].printBill();
     }
 }
